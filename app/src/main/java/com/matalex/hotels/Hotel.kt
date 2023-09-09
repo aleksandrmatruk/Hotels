19b5e7d806a030018ab8ae5ecdf05522dbbe1962
Aleksandr Matruk <matruk.aleksandr@gmail.com>
2023-09-09 18:25:18 +0300

    - с глюком

--- app/src/main/java/com/matalex/hotels/Hotel.kt
+++ app/src/main/java/com/matalex/hotels/Hotel.kt
@@ -5,14 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.matalex.hotels.databinding.FragmentHotelBinding
 
 class Hotel : Fragment() {
+
+    lateinit var binding: FragmentHotelBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentHotelBinding.inflate(inflater, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hotel, container, false)
+        return binding.root
     }
 }
