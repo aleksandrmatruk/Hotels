0cf8e8b59a3d2f9abdb0d90001a952d0bba9a871
Aleksandr Matruk <matruk.aleksandr@gmail.com>
2023-09-09 17:49:48 +0300

    - всё пропало, Шеф

--- app/src/main/java/com/matalex/hotels/MainActivity.kt
+++ app/src/main/java/com/matalex/hotels/MainActivity.kt
@@ -2,10 +2,13 @@ package com.matalex.hotels
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.matalex.hotels.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
