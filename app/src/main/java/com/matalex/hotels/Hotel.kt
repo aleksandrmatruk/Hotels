f1328b9c30d8ab64e13612976e092e57a10afc31
Aleksandr Matruk <matruk.aleksandr@gmail.com>
2023-09-10 21:30:41 +0300

    - Разрешение интернета - добавил библиотеку Volley - функция получения данных

--- app/src/main/java/com/matalex/hotels/Hotel.kt
+++ app/src/main/java/com/matalex/hotels/Hotel.kt
@@ -1,10 +1,14 @@
 package com.matalex.hotels
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.matalex.hotels.databinding.FragmentHotelBinding
 
 class Hotel : Fragment() {
@@ -16,6 +20,26 @@ class Hotel : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentHotelBinding.inflate(inflater, container, false)
+
+        getDataFromApi()
+
         return binding.root
+    }
+
+    private fun getDataFromApi() {
+
+        val url = "https://run.mocky.io/v3/35e0d18e-2521-4f1b-a575-f0fe366f66e3"
+        val queue = Volley.newRequestQueue(context)
+        val stringRequest = StringRequest(Request.Method.GET,
+            url,
+            { response ->
+                Log.d("MyLog", "Response: $response")
+            },
+            {
+                Log.d("MyLog", "Volley error: $it")
+            }
+        )
+        queue.add(stringRequest)
+
     }
 }
